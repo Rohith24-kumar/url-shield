910b0cb439508ba4d46197ca850c4d5a9a1f269e
Rohith24-kumar <rohithyennehole25@gmail.com>
2026-06-19 14:55:12 +0530

    api hardcode

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = "https://url-shield-ujy5.onrender.com/api"
 
 // ─── Real API calls ───────────────────────────────────────────────────────────
 
